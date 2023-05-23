54d68dc49cb5495820855a9d718a2373c3c6e6fb
Gabriel Guilbee-Rodriguez <guilbeeg@mail.gvsu.edu>
2023-05-23 05:11:53 -0700

    Update script

---
+++ config.hcl
@@ -0,0 +1,4 @@
+cluster_addr  = "https://vault-server.hashicorp.com:8201"
+api_addr      = "https://vault-server.hashicorp.com:8200"
+disable_mlock = true
+
--- setup.sh
+++ setup.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-# Set environment variable with Github username in /root/.bash_profile
-echo 'export GITHUB_USERNAME="shruthivar"' >> /root/.bash_profile
+#Sets an environment variable with your github username in /root/.bash_profile
+echo 'export GIT_USER="guilbeeg"' >> ~/.bash_profile
 
-# Write config.hcl with the specified code and replace <HOSTNAME> value
-echo 'cluster_addr  = "https://<HOSTNAME>:8201"' > config.hcl
-echo 'api_addr      = "https://<HOSTNAME>:8200"' >> config.hcl
-echo 'disable_mlock = true' >> config.hcl
-sed -i 's/<HOSTNAME>/vault-server.hashicorp.com/g' config.hcl
+#Confirm env var is set
+cat ~/.bash_profile
+
+# Replace <HOSTNAME> with "vault-server.hashicorp.com"
+HOSTNAME="vault-server.hashicorp.com"
+
+# Write the following configuration parameters to config.hcl
+echo 'cluster_addr  = "https://'"$HOSTNAME"':8201"' >> config.hcl
+echo 'api_addr  = "https://'"$HOSTNAME"':8200"' >> config.hcl
+echo 'disable_mlock  = true' >> config.hcl
+
+#Confirm values are correct
+cat config.hcl
