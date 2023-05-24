0dfd783e09533a0397ef806b595d4d37e684f123
Gabriel Guilbee <guilbeeg@mail.gvsu.edu>
2023-05-23 20:42:14 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,16 +3,13 @@
 #Sets an environment variable with your github username in /root/.bash_profile
 echo 'export GIT_USER="guilbeeg"' >> ~/.bash_profile
 
-#Confirm env var is set
-cat ~/.bash_profile
-
-# Replace <HOSTNAME> with "vault-server.hashicorp.com"
-HOSTNAME="vault-server.hashicorp.com"
-
 # Write the following configuration parameters to config.hcl
-echo 'cluster_addr  = "https://'"$HOSTNAME"':8201"' >> config.hcl
-echo 'api_addr  = "https://'"$HOSTNAME"':8200"' >> config.hcl
-echo 'disable_mlock  = true' >> config.hcl
+echo 'cluster_addr  = "https://<HOSTNAME>:8201"' > config.hcl
+echo 'api_addr      = "https://<HOSTNAME>:8200"' >> config.hcl
+echo 'disable_mlock = true' >> config.hcl
+
+#Change <HOSTNAME> to "vault-server.hashicorp.com" & create a backup file
+sed -i'.backup' -e 's/<HOSTNAME>/vault-server.hashicorp.com/g' config.hcl
 
 #Confirm values are correct
 cat config.hcl
